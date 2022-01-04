be1107b25459cdbdc389b78ed72fdf3aa47b3668
CampbellKT95 <campbellkt2013@gmail.com>
2022-01-03 20:24:24 -0500

    finished accordion functionality. need to add same funcitonality to arrows on click, make final touch-ups to mobile version, and create desktop version

--- faq-accordion/scripts.js
+++ faq-accordion/scripts.js
@@ -5,13 +5,21 @@ const questions = document.querySelectorAll(".questions");
 //maybe grab parent element and look for child from there?
 
 function expandAccordion(e) {
-    const description = e.currentTarget.parentNode.children[2];
-    if (description.className === "faq-answer-container-hidden") {
-        description.className = "faq-answer-container-active"
-    } else if (description.className === "faq-answer-container-active") {
-        description.className = "faq-answer-container-hidden"   
+    const question = e.currentTarget;
+    const answer = e.currentTarget.parentNode.children[2];
+    const arrow = e.currentTarget.parentNode.children[1];
+    console.log(arrow.className)
+
+    if (answer.className === "faq-answer-container-hidden") {
+        answer.className = "faq-answer-container-active";
+        question.style.fontWeight = "700";
+        arrow.className = "arrow-icon-active";
+    } else if (answer.className === "faq-answer-container-active") {
+        answer.className = "faq-answer-container-hidden";
+        question.style.fontWeight = "400";   
+        arrow.className = "arrow-icon-unactive";
     } else {
-        console.log("an error has occured");
+        answer.log("an error has occured");
     };
 };
 
