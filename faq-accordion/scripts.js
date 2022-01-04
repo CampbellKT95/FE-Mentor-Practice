3cea19d696ffe3286eaf950cecb487429254bbda
CampbellKT95 <campbellkt2013@gmail.com>
2022-01-03 20:31:48 -0500

    fixed bug where arrow activation of function did not change fontWeight of question

--- faq-accordion/scripts.js
+++ faq-accordion/scripts.js
@@ -1,14 +1,11 @@
 
 const questions = document.querySelectorAll(".questions");
-
-//create function that causes .faq-answer-container sibling display="inline-block"
-//maybe grab parent element and look for child from there?
+const arrows = document.querySelectorAll(".arrow");
 
 function expandAccordion(e) {
-    const question = e.currentTarget;
+    const question = e.currentTarget.parentNode.children[0];
     const answer = e.currentTarget.parentNode.children[2];
     const arrow = e.currentTarget.parentNode.children[1];
-    console.log(arrow.className)
 
     if (answer.className === "faq-answer-container-hidden") {
         answer.className = "faq-answer-container-active";
@@ -19,10 +16,14 @@ function expandAccordion(e) {
         question.style.fontWeight = "400";   
         arrow.className = "arrow-icon-unactive";
     } else {
-        answer.log("an error has occured");
+        console.log("an error has occured");
     };
 };
 
 for (let i = 0; i < questions.length; i++) {
     questions[i].addEventListener("click", expandAccordion);
 };
+
+for (let i = 0; i < arrows.length; i++) {
+    arrows[i].addEventListener("click", expandAccordion);
+}
