1bd3ac67ca0314253a92093732f87944ef37f831
CampbellKT95 <campbellkt2013@gmail.com>
2022-01-03 20:12:55 -0500

    successfully added accordion, now need to style. need to change question's font-weight and add transitions to make accordion smooth

--- faq-accordion/scripts.js
+++ faq-accordion/scripts.js
@@ -1,9 +1,20 @@
 
 const questions = document.querySelectorAll(".questions");
 
-//create function that causes .faq-answer-container child display="inline-block"
+//create function that causes .faq-answer-container sibling display="inline-block"
+//maybe grab parent element and look for child from there?
 
+function expandAccordion(e) {
+    const description = e.currentTarget.parentNode.children[2];
+    if (description.className === "faq-answer-container-hidden") {
+        description.className = "faq-answer-container-active"
+    } else if (description.className === "faq-answer-container-active") {
+        description.className = "faq-answer-container-hidden"   
+    } else {
+        console.log("an error has occured");
+    };
+};
 
 for (let i = 0; i < questions.length; i++) {
-    questions[i].addEventListener("click", console.log("click added"))
-}
+    questions[i].addEventListener("click", expandAccordion);
+};
