89cd108f50c2c1340733cb7f5d325ab14bc007dd
CampbellKT95 <campbellkt2013@gmail.com>
2022-01-11 19:32:59 -0500

    continued work on filter logic

--- to-do/filter.js
+++ to-do/filter.js
@@ -1,4 +1,11 @@
-//will filter out all/active/completed using renderedItems
+const all = document.querySelector("#filter-all");
+all.addEventListener("click", () => filterAll());
+
+const active = document.querySelector("#filter-active");
+active.addEventListener("click", () => filterActive());
+
+const completed = document.querySelector("#filter-completed")
+completed.addEventListener("click", () => filterComplete());
 
 function filterAll() {
     renderListItems();
@@ -6,9 +13,23 @@ function filterAll() {
 
 function filterActive() {
     //map through all items with status: false to render
-}
+    const filterIncomplete = renderedItems.filter((item) => {
+        return item.status === false;
+    });
+    console.log(filterIncomplete);
+    filterIncomplete.map((item) => {
+        createListItems(item);
+    });
+};
 
 function filterComplete() {
     //map through all items with status: true to render
-}
+    const filterCompleted = renderedItems.filter((item) => {
+        return item.status === true;
+    });
+    console.log(filterCompleted)
+    filterCompleted.map((item) => {
+        createListItems(item);
+    });
+};
 
