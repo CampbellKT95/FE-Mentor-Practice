4d37fed4da80ef37f6136da355473cd2bb9afeb8
CampbellKT95 <campbellkt2013@gmail.com>
2022-01-13 10:14:44 -0500

    figured out issue with theme change

--- to-do/theme.js
+++ to-do/theme.js
@@ -1,6 +1,5 @@
 //icon of sun/moon for light/dark
 const themeIcon = document.querySelector("#theme-icon");
-console.log(themeIcon)
 let theme = "light";
 themeIcon.addEventListener("click", () => {
     if (theme === "light") {
@@ -20,13 +19,15 @@ const bannerImage = document.querySelector(".header-container");
 const createNewItem = document.querySelector(".create-to-do");
 
 //the rendered listItems
-const listItemsWrapper = document.querySelector(".list-items-container");
+const listItemsContainer = document.querySelector(".list-items-container");
+
+const listItemsWrapper = document.querySelector(".list-item-wrapper");
+console.log(listItemsWrapper)
 
 const clearCompletedWrapper = document.querySelector(".number-clear-wrapper");
 
 //final wrapper @ the bottom with filter options
 const filterWrappers = document.querySelector(".filters-wrapper");
-console.log(filterWrappers)
 
 function darkTheme() {
     body.style.backgroundColor = "hsl(235, 21%, 11%)";
@@ -38,9 +39,8 @@ function darkTheme() {
     createNewItem.style.backgroundColor = "hsl(233, 14%, 35%)";
     createNewItem.style.color = "hsl(234, 39%, 85%)";
 
-    listItemsWrapper.style.backgroundColor = "hsl(233, 14%, 35%)";
+    listItemsContainer.style.backgroundColor = "hsl(233, 14%, 35%)";
     listItemsWrapper.style.color = "white";
-
 
     clearCompletedWrapper.style.backgroundColor = "hsl(233, 14%, 35%)";
     clearCompletedWrapper.style.color = "white";
@@ -48,7 +48,6 @@ function darkTheme() {
     filterWrappers.style.backgroundColor = "hsl(233, 14%, 35%)";
 
     theme = "dark";
-    console.log("darkened")
 };
 
 
@@ -61,7 +60,7 @@ function lightTheme() {
 
     createNewItem.style.backgroundColor = "white";
 
-    listItemsWrapper.style.color = "hsl(237, 14%, 26%)";
+    listItemsContainer.style.color = "hsl(237, 14%, 26%)";
     listItemsWrapper.style.backgroundColor = "white";
 
     clearCompletedWrapper.style.backgroundColor = "white";
@@ -70,5 +69,4 @@ function lightTheme() {
     filterWrappers.style.backgroundColor = "white";
 
     theme = "light";
-    console.log("lightened")
 };
