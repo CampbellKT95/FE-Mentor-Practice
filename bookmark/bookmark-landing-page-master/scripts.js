f964d0461e24a97dcdf421cb9c286c3b3ad9e014
CampbellKT95 <campbellkt2013@gmail.com>
2022-01-17 10:32:56 -0500

    added dropdown functionality, now need to make text appear more smoothly & change arrow color/direction along with slide

--- bookmark/bookmark-landing-page-master/scripts.js
+++ bookmark/bookmark-landing-page-master/scripts.js
@@ -11,3 +11,16 @@ openModal.addEventListener("click", () => {
 closeModal.addEventListener("click", () => {
     modal.style.display = "none";
 });
+//
+
+//faq
+allFaqQuestions = document.querySelectorAll(".faq-question-wrapper");
+allFaqAnswers = document.querySelectorAll(".faq-answer-wrapper");
+
+for (let i = 0; i < allFaqQuestions.length; i++) {
+        allFaqQuestions[i].addEventListener("click", () => {
+        console.log("clicked")
+        allFaqAnswers[i].className = "faq-answer-wrapper-active";
+    });
+};
+//
