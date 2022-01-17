c8b59b3bfe7d826e967bb4b113943aec4035aaf9
CampbellKT95 <campbellkt2013@gmail.com>
2022-01-17 10:12:15 -0500

    mobile navbar dropdown modal complete

--- bookmark/bookmark-landing-page-master/scripts.js
+++ bookmark/bookmark-landing-page-master/scripts.js
@@ -2,5 +2,12 @@
 //mobile modal
 modal = document.querySelector("#modal");
 openModal = document.querySelector("#open-modal");
+closeModal = document.querySelector(".close-modal-icon");
 
+openModal.addEventListener("click", () => {
+    modal.style.display = "block";
+});
 
+closeModal.addEventListener("click", () => {
+    modal.style.display = "none";
+});
